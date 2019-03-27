2aada01588431624b2fe58c5882cc3d14b60c49e
feng.zhang <cst.feng@gmail.com>
2019-03-27 17:41:10 +0900

    open option page if options have not been setup

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
-chrome.storage.sync.get(['samlUrl'], function(items){
-  var samlUrl = items.samlUrl;
-
-  chrome.browserAction.onClicked.addListener(function(activeTab) {
-    chrome.tabs.create({url: samlUrl});
+chrome.browserAction.onClicked.addListener(function(activeTab) {
+  chrome.storage.sync.get(['samlUrl', 'googleAccount', 'awsAccount'], function(items){
+    if(items.samlUrl && items.googleAccount && items.awsAccount) {
+      chrome.tabs.create({url: items.samlUrl});
+    }
+    else {
+      console.log("Options need to be setup");
+      chrome.tabs.create({url: "options.html"});
+    }
   });
 });
