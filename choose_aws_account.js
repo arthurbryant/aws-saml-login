4c1cb1c03b8deb8794c3f1aaec44f74bb6115809
feng.zhang <cst.feng@gmail.com>
2019-03-29 15:32:46 +0900

    do nothing if there are multiple aws accounts and option not set

--- choose_aws_account.js
+++ choose_aws_account.js
@@ -1,15 +1,12 @@
 chrome.storage.sync.get(['awsAccount'], function(items) {
-  var patten = new RegExp(items.awsAccount + ".*");
   var titles = document.getElementsByClassName("saml-account-name");
-  var index;
+  var index = 0;
 
-  if(titles.length == 1) {
-    index = 0;
-  }
-  else {
+  if(titles.length > 1) {
+    var pattern = new RegExp(items.awsAccount + ".*");
     for(var i = 0; i < titles.length; ++i) {
       var text = titles[i].textContent;
-      if (patten.test(text)) {
+      if (pattern.test(text)) {
         index = i;
         console.log("Found aws accout: " + items.awsAccount);
 
@@ -18,9 +15,11 @@ chrome.storage.sync.get(['awsAccount'], function(items) {
     }
   }
 
-  var aws_accounts = document.getElementsByClassName("saml-account");
-  var checkbox = aws_accounts[index * 2 + 1].getElementsByTagName("input")[0];
+  if(items.awsAccount) {
+    var aws_accounts = document.getElementsByClassName("saml-account");
+    var checkbox = aws_accounts[index * 2 + 1].getElementsByTagName("input")[0];
 
-  checkbox.checked = true;
-  document.getElementById("signin_button").click();
+    checkbox.checked = true;
+    document.getElementById("signin_button").click();
+  }
 });
