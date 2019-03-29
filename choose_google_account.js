5aca7ac1f5b587154138ce9c5b4cca0fdc79fb7f
feng.zhang <cst.feng@gmail.com>
2019-03-29 15:32:19 +0900

    do nothing if there are multiple google accounts and option not set

--- choose_google_account.js
+++ choose_google_account.js
@@ -1,15 +1,16 @@
 chrome.storage.sync.get(['googleAccount'], function(items){
-  var patten = new RegExp(items.googleAccount);
   var btns = document.getElementsByTagName("button");
-
   if(btns.length == 1) {
     btns[0].click();
-    return;
   }
-  for(var i = 0; i < btns.length; ++i) {
-    if(patten.test(btns[i].value)) {
-      btns[i].click();
+  else {
+    var patten = new RegExp(items.googleAccount);
+    if(items.googleAccount) {
+      for(var i = 0; i < btns.length; ++i) {
+        if(patten.test(btns[i].value)) {
+          btns[i].click();
+        }
+      }
     }
   }
 });
-
