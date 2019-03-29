b7be2a50ad65f293f1c3e26b70b00863ac64dbd0
feng.zhang <cst.feng@gmail.com>
2019-03-29 14:48:21 +0900

    choose the only google account if there is only one

--- choose_google_account.js
+++ choose_google_account.js
@@ -1,6 +1,11 @@
 chrome.storage.sync.get(['googleAccount'], function(items){
   var patten = new RegExp(items.googleAccount);
   var btns = document.getElementsByTagName("button");
+
+  if(btns.length == 1) {
+    btns[0].click();
+    return;
+  }
   for(var i = 0; i < btns.length; ++i) {
     if(patten.test(btns[i].value)) {
       btns[i].click();
