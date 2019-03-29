d25561c3886487a702ce6d1f5d4f28bf7425fa5e
feng.zhang <cst.feng@gmail.com>
2019-03-29 14:48:49 +0900

    choose the only aws account if there is only one

--- choose_aws_account.js
+++ choose_aws_account.js
@@ -1,23 +1,26 @@
-chrome.storage.sync.get(['awsAccount'], function(items){
+chrome.storage.sync.get(['awsAccount'], function(items) {
   var patten = new RegExp(items.awsAccount + ".*");
   var titles = document.getElementsByClassName("saml-account-name");
   var index;
-  for(var i = 0; i < titles.length; ++i) {
-    var text = titles[i].textContent;
-    if (patten.test(text)) {
-      index = i;
-      break;
+
+  if(titles.length == 1) {
+    index = 0;
+  }
+  else {
+    for(var i = 0; i < titles.length; ++i) {
+      var text = titles[i].textContent;
+      if (patten.test(text)) {
+        index = i;
+        console.log("Found aws accout: " + items.awsAccount);
+
+        break;
+      }
     }
   }
 
-  if(typeof index == "undefined") {
-    console.log("Couldn't find aws accout: " + items.awsAccount);
-  }
-  else {
-    var aws_accounts = document.getElementsByClassName("saml-account");
-    var checkbox = aws_accounts[index * 2 + 1].getElementsByTagName("input")[0];
+  var aws_accounts = document.getElementsByClassName("saml-account");
+  var checkbox = aws_accounts[index * 2 + 1].getElementsByTagName("input")[0];
 
-    checkbox.checked = true;
-    document.getElementById("signin_button").click();
-  }
+  checkbox.checked = true;
+  document.getElementById("signin_button").click();
 });
