c036a6187885dd483801c1b61f8ca4530ffb030a
feng.zhang <cst.feng@gmail.com>
2021-03-29 20:50:36 +0900

    refactor: choose_google_account.js

--- choose_google_account.js
+++ choose_google_account.js
@@ -1,17 +1,22 @@
-chrome.storage.sync.get(['googleAccount'], function(items){
+chrome.storage.sync.get(['googleAccount'], function(items) {
   let loginElements = document.querySelectorAll('[data-identifier]');
+
   if(loginElements.length == 1) {
     loginElements[0].click();
+    return true;
   }
-  else {
-    let patten = new RegExp(items.googleAccount);
-    if(items.googleAccount) {
-      for(let i = 0; i < loginElements.length; ++i) {
-        let email = loginElements[i].getAttribute("data-identifier");
-        if(patten.test(email)) {
-          loginElements[i].click();
-        }
+
+  let targetGoogleAccount = items.googleAccount;
+  if(targetGoogleAccount) {
+    for(let i = 0; i < loginElements.length; ++i) {
+      let le = loginElements[i];
+      let email = le.getAttribute("data-identifier");
+      if(targetGoogleAccount === email) {
+        le.click();
+        return true
       }
     }
   }
+
+  return false;
 });
